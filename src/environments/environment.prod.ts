b3a9deee64839d338b27931abaf0c53447f03267
Nahshon <nahshonrm@gmail.com>
2019-06-12 00:23:15 +0300

    pervii

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCBRLVyFkP736lHPKMLAvWGw9CqOj64V4k',
+    authDomain: 'organic-shop-24d04.firebaseapp.com',
+    databaseURL: 'https://organic-shop-24d04.firebaseio.com',
+    projectId: 'organic-shop-24d04',
+    storageBucket: 'organic-shop-24d04.appspot.com',
+    messagingSenderId: '372605044428',
+    appId: '1:372605044428:web:5ddb1593f5739622'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCBRLVyFkP736lHPKMLAvWGw9CqOj64V4k',
+    authDomain: 'organic-shop-24d04.firebaseapp.com',
+    databaseURL: 'https://organic-shop-24d04.firebaseio.com',
+    projectId: 'organic-shop-24d04',
+    storageBucket: 'organic-shop-24d04.appspot.com',
+    messagingSenderId: '372605044428',
+    appId: '1:372605044428:web:5ddb1593f5739622'
+  }
 };
 
 /*
